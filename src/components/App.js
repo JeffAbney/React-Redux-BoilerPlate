55a85ca9bccb2d3cf93141e71ef9955633476522
JeffAbney <jeffmabney@gmail.com>
2019-10-16 12:18:30 -0500

    remove bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,9 @@ import ConnectedComponent from "./ConnectedComponent.js";
 
 
 const App = () => (
-  <div className="row mt-5">
-	<div className="col-md-4 offset-md-1">
+  <div>
 	  <h2>Hello There!</h2>
 	  <ConnectedComponent />
-	</div>
   </div>
 );
 
